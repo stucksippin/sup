c44c870b570269f479caf54bb84f372b9ab58853
sippin <regulationqq@mail.ru>
2026-05-26 10:40:43 +0300

    trust host

--- auth.ts
+++ auth.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 import { z } from "zod";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+    trustHost: true,
     session: { strategy: "jwt" },
     pages: {
         signIn: "/login",
